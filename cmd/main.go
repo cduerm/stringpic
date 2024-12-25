3d4153dab4854985dec02867330d8f6b067cea15
Christoph Dürmann <christoph@duermann.de>
2024-12-25 14:51:13 +0100

    reorderd project structure

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
+	"path"
+	"strings"
 
 	"github.com/cduerm/stringpic/stringer"
 )
@@ -16,11 +18,13 @@ var paddingPixel = 0
 var outputSize = 512
 var nLines = 2000
 var diameterMeter float64 = 0.226
+var outDir = "output"
 
 var stringDarkness = uint8(max(1, min(255, 20*(float64(outputSize)/400)*(2500/float64(nLines)))))
 
 func init() {
 	flag.StringVar(&filename, "filename", filename, "png file to convert to string art")
+	flag.StringVar(&outDir, "output", outDir, "directory where to put the output files")
 	flag.IntVar(&pinCount, "pinCount", pinCount, "number of pins in circular pattern")
 	flag.IntVar(&outputSize, "size", outputSize, "size of output image")
 	flag.IntVar(&nLines, "nLines", nLines, "number of lines")
@@ -39,12 +43,13 @@ func main() {
 
 	instructions, length := stringer.Generate(targetImage, resultImage, allLines, nLines, stringDarkness, diameterMeter)
 
-	err = stringer.WriteInstructionsToDisk("instructions.txt", instructions, length)
+	outFilenameBase, _ := strings.CutSuffix(path.Base(filename), path.Ext(filename))
+	err = stringer.WriteInstructionsToDisk(path.Join(outDir, outFilenameBase+"_instructions.txt"), instructions, length)
 	if err != nil {
 		panic(err)
 	}
 
-	err = stringer.SaveImageToDisk("out.png", resultImage)
+	err = stringer.SaveImageToDisk(path.Join(outDir, outFilenameBase+"_stringer.png"), resultImage)
 	if err != nil {
 		panic(err)
 	}
